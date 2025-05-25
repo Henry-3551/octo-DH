83600e754d09ec965889e89a74ba6d01c7aaf0aa
Ani Henry Chukwuemerie <104362482+Henry-3551@users.noreply.github.com>
2025-05-25 19:35:51 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('tts-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './sw.js',
+        './icon-192.png',
+        './logo-icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
